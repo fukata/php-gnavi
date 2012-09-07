7d6e0a367db679c61971142a1a71b234bdbdb7c4
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2012-09-07 14:44:46 +0900

    各インタフェースを実装

--- lib/gnavi/Gnavi.php
+++ lib/gnavi/Gnavi.php
@@ -1,5 +1,9 @@
 <?php
 require_once __DIR__ . '/GnaviRestSearchApi.php';
+require_once __DIR__ . '/GnaviAreaSearchApi.php';
+require_once __DIR__ . '/GnaviPrefSearchApi.php';
+require_once __DIR__ . '/GnaviCategoryLargeSearchApi.php';
+require_once __DIR__ . '/GnaviCategorySmallSearchApi.php';
 
 class Gnavi {
 
@@ -13,4 +17,25 @@ class Gnavi {
         $api = new GnaviRestSearchApi($this->apikey);
         return $api->execute($query);
     }
+
+    public function doAreaSearchApi($query=array()) {
+        $api = new GnaviAreaSearchApi($this->apikey);
+        return $api->execute($query);
+    }
+
+    public function doPrefSearchApi($query=array()) {
+        $api = new GnaviPrefSearchApi($this->apikey);
+        return $api->execute($query);
+    }
+
+    public function doCategoryLargeSearchApi($query=array()) {
+        $api = new GnaviCategoryLargeSearchApi($this->apikey);
+        return $api->execute($query);
+    }
+
+    public function doCategorySmallSearchApi($query=array()) {
+        $api = new GnaviCategorySmallSearchApi($this->apikey);
+        return $api->execute($query);
+    }
+
 }
